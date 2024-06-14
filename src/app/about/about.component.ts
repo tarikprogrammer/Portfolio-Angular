38193572ad287694529a2e8eb318a1b2aac34d88
tarikprogrammer <belaid.tarikk@gmail.com>
2024-06-14 19:52:32 +0100

    first push

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,12 +8,7 @@ import Typed from "typed.js";
 })
 export class AboutComponent implements OnInit{
     ngOnInit(): void {
-       const options={
-         strings:['My name is Tarik Belaid , I\'m 22 years old after obtaining my baccalaureate I decided to continue my study at ENSA MARRAKESH in computer science .'],
-         typeSpeed:50,
-         loop:true
-       }
-       const type=new Typed('.about-text',options);
+
     }
 
 }
